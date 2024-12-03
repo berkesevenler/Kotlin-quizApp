5f7688c8b6adde34c6c1afbe0bf8b2349f3894a8
berkesevenler <berkeseveenlr@gmail.com>
2024-12-03 18:52:10 +0100

    edit info bars

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
         Handler(Looper.getMainLooper()).postDelayed(3000){
             val intent= Intent(this,LoginActivity::class.java)
             startActivity(intent)
